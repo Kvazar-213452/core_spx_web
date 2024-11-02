186c63090f42522570829265cc1a359d0b7bff7b
Conar365 <kvazar382@gmail.com>
2024-11-02 15:43:13 +0300

    end NM2

--- shell_web_NM1/README.md
+++
@@ -1,4 +0,0 @@
-# core_spx V - 5.6
-compilete command -
-gcc main.c -o shell_web.exe -L. -lwebview -mwindows
-start command - ./shell_web inst 400 400 "3000"
--- shell_web_NM2/main.py
+++ shell_web_NM2/main.py
@@ -5,6 +5,7 @@ import sys
 width = sys.argv[1]
 height = sys.argv[2]
 port_data = sys.argv[3]
+name_ = sys.argv[4]
 
 find_free_port_dll = ctypes.CDLL('./FindFreePort.dll')
 find_free_port_dll.FindFreePort.restype = ctypes.c_int
@@ -22,6 +23,10 @@ def icon_data():
     with open('icon.data', 'r') as file:
         base64_data = file.read()
     return base64_data
+
+@eel.expose
+def name():
+    return name_
 
 eel.init('web')
 
